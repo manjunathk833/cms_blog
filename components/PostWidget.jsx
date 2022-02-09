8af5d1da2b38542085113b2a2bd61334b23f0380
manjunath h k <manjunathhk833@gmail.com>
2022-02-09 20:46:00 +0530

    completed related posts in post widget component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -1,12 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import moment from 'moment';
 import Link from 'next/link';
+import { Result } from 'postcss';
 
+import { getRecentPosts, getSimilarPosts } from '../services';
+const PostWidget = ({categories, slug}) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
 
-const PostWidget = () => {
+  useEffect(() => {
+    if(slug){
+      getSimilarPosts(categories, slug)
+        .then((result) => setRelatedPosts(result))
+    } else {
+      getRecentPosts()
+        .then((result) => setRelatedPosts(result))
+    }
+  }, [slug]);
+  
+  console.log(relatedPosts);
+
   return (
-        <div>
-            PostWidget
+        <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+            <h3 className='text-xl font-semibold border-b pb-4'>
+              {slug ? 'Related Posts' : "Related Posts"}
+            </h3> 
+            {relatedPosts.map((post) => (
+              <div key={post.title} className='flex items-center w-full mb-4'>
+                <div className='w-16 flex-none'>
+                   <img 
+                      alt={post.title}
+                      height="60px"
+                      width="60px"
+                      className='align-middle rounded-full'
+                      src={post.featuredImage.url}
+                   />
+                </div>  
+                <div className='flex-grow ml-4'>
+                  <p className='text-gray-500 font-xs'>
+                    {moment(post.createdAt).format('MMM DD, YYYY')}
+                  </p>
+                  <Link href={`/post/${post.slug}`} key={post.title} className="text-md">
+                    {post.title}
+                  </Link>
+                </div>
+              </div>
+            ))}
         </div>
   );
 };
